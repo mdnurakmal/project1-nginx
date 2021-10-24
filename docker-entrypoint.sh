2e9f76719cf2eb2e9c15a93cc3d92d8a9aa016ed
Nur Akmal <mdnurakmal@gmail.com>
2021-10-25 01:33:27 +0800

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env sh
 set -eu
 
-echo hello
+echo hello $1
+WEB_URL=$1
 
-envsubst '${1}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/nginx.conf
+envsubst '${WEB_URL}' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/nginx.conf
 
 #exec "$@"
